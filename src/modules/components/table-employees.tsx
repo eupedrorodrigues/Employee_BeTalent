cc8f7a535ee1453d01c36420ab8557a59c52b985
Pedro Rodrigues <pedro.rodrigues@compasso.com.br>
2025-02-28 11:07:24 -0300

    style: refine layout styles for improved responsiveness

--- src/modules/components/table-employees.tsx
+++ src/modules/components/table-employees.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import React from "react";
-import { useState } from "react";
-
+import React, { useState } from "react";
 import { Circle, ArrowDown } from "@/constants";
 import { TableEmployeesProps } from "@/types/employee";
 import { formatDate, formatNumber } from "@/utils/formatters";
@@ -15,97 +13,102 @@ export const TableEmployees = ({ employees }: TableEmployeesProps) => {
   };
 
   return (
-    <>
-      <div className="w-full block min-h-[32em]">
-        <table className="w-full border-collapse shadow-md">
-          <thead>
-            <tr className="bg-[#0500FF] text-white shadow-md h-[2.9375rem]">
-              <th className="pl-5 md:pl-10 rounded-tl-lg text-left">FOTO</th>
-              <th className=" p-3 md:pr-0 text-left md:rounded-none rounded-tr-lg">
-                <div className="flex items-center justify-between w-[13em] md:w-full">
-                  NOME
-                  <div className="md:hidden">
-                    <span>
-                      <img src={Circle.icon} alt={Circle.title} />
-                    </span>
-                  </div>
-                </div>
-              </th>
-              <th className="text-left hidden md:table-cell">CARGO</th>
-              <th className="pl-10 text-left hidden md:table-cell ">
-                DATA DE ADMISSÃO
-              </th>
-              <th className=" rounded-tr-lg text-left hidden md:table-cell">
-                TELEFONE
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {employees.length === 0 ? (
-              <tr className="border-b border-[#cdcdcd] border-2 bg-white">
+    <div className="w-full block min-h-[32em]">
+      <table className="w-full border-collapse shadow-md">
+        <thead>
+          <tr className="bg-[#0500FF] text-white shadow-md h-[2.9375rem]">
+            <th className="rounded-tl-lg pl-5 md:pl-15">FOTO</th>
+            <th className="pr-5 md:pr-0  md:rounded-none rounded-tr-lg">
+              <div className="flex items-center justify-between w-[13em] md:w-full">
+                NOME
+                <span className="md:hidden pr-2">
+                  <img src={Circle.icon} alt={Circle.title} />
+                </span>
+              </div>
+            </th>
+            <th className="hidden md:table-cell">CARGO</th>
+            <th className=" pr-5 hidden md:table-cell">DATA DE ADMISSÃO</th>
+            <th className="rounded-tr-lg hidden md:table-cell">TELEFONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.length === 0 ? (
+            <>
+              <tr className="border-b border-[#cdcdcd] bg-white">
                 <td colSpan={5} className="text-center py-4 text-black">
                   Usuário não encontrado
                 </td>
               </tr>
-            ) : (
-              employees.map((employee, index) => (
-                <React.Fragment key={index}>
-                  <tr
-                    className="border-b border-[#cdcdcd] bg-white hover:bg-gray-100"
-                    onClick={() => toggleAccordion(index)}
-                  >
-                    <td className="py-2 pl-5 md:pl-10">
+            </>
+          ) : (
+            employees.map((employee, index) => (
+              <React.Fragment key={index}>
+                <tr
+                  className="border-b border-[#cdcdcd] bg-white hover:bg-gray-100"
+                  onClick={() => toggleAccordion(index)}
+                >
+                  <td className="py-2 pl-5 md:pl-15">
+                    <img
+                      src={employee.image}
+                      alt="Employee Screen"
+                      className="w-[2.125em] h-[2.125em] rounded-full"
+                    />
+                  </td>
+                  <td className="text-black text-left relative">
+                    <span className="align-middle">{employee.name}</span>
+                    <span
+                      className={`md:hidden absolute right-5 top-1/2 -translate-y-1/2 transition-transform duration-200 transform origin-center ${
+                        openIndex === index ? "rotate-180" : "rotate-0"
+                      }`}
+                    >
                       <img
-                        src={employee.image}
-                        alt="Foto"
-                        className="w-[2.125em] h-[2.125em] rounded-full"
+                        src={ArrowDown.icon}
+                        alt={ArrowDown.title}
+                        className="align-middle"
                       />
-                    </td>
-                    <td className="flex items-center justify-between pt-4 pl-3 text-black text-left">
-                      {employee.name}
-                      <div className="md:hidden flex items-center pr-5">
-                        <span
-                          className={`transition-transform duration-200 transform origin-center ${
-                            openIndex === index ? "rotate-180" : "rotate-0"
-                          }`}
-                        >
-                          <img src={ArrowDown.icon} alt={ArrowDown.title} />
-                        </span>
-                      </div>
-                    </td>
-                    <td className="text-black text-left hidden md:table-cell">
-                      {employee.job}
-                    </td>
-                    <td className="pl-10 text-black text-left hidden md:table-cell">
-                      {formatDate(employee.admission_date)}
-                    </td>
-                    <td className="text-black text-left hidden md:table-cell">
-                      {formatNumber(employee.phone)}
-                    </td>
-                  </tr>
-                  {openIndex === index && (
-                    <tr className="bg-gray-50 text-black md:hidden ">
-                      <td colSpan={5} className="p-5 flex-row">
-                        <p className="flex justify-between border-b border-dotted">
-                          <strong>Cargo</strong> {employee.job}
-                        </p>
-                        <p className="flex justify-between pt-3 border-b border-dotted">
-                          <strong>Data de Admissão</strong>
-                          {formatDate(employee.admission_date)}
-                        </p>
-                        <p className="flex justify-between pt-3 border-b border-dotted">
-                          <strong>Telefone</strong>{" "}
-                          {formatNumber(employee.phone)}
-                        </p>
-                      </td>
-                    </tr>
-                  )}
-                </React.Fragment>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
-    </>
+                    </span>
+                  </td>
+
+                  <td className="text-left text-black hidden md:table-cell">
+                    {employee.job}
+                  </td>
+                  <td className="text-left text-black hidden md:table-cell">
+                    {formatDate(employee.admission_date)}
+                  </td>
+                  <td className="text-left text-black hidden md:table-cell">
+                    {formatNumber(employee.phone)}
+                  </td>
+                </tr>
+
+                <tr className="bg-white text-black md:hidden">
+                  <td colSpan={5} className="p-0">
+                    <div
+                      className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                        openIndex === index
+                          ? "max-h-[500px] opacity-100 py-4"
+                          : "max-h-0 opacity-0 py-0"
+                      }`}
+                    >
+                      <p className="flex justify-between border-b border-[#dfdfdf] border-dotted px-5">
+                        <span className="font-medium">Cargo</span>{" "}
+                        {employee.job}
+                      </p>
+                      <p className="flex justify-between border-b border-[#dfdfdf] pt-3 border-dotted px-5">
+                        <span className="font-medium">Data de Admissão</span>
+                        {formatDate(employee.admission_date)}
+                      </p>
+                      <p className="flex justify-between border-b border-[#dfdfdf] pt-3 px-5">
+                        <span className="font-medium">Telefone</span>{" "}
+                        {formatNumber(employee.phone)}
+                      </p>
+                    </div>
+                  </td>
+                </tr>
+              </React.Fragment>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
--- src/modules/pages/index.tsx
+++ src/modules/pages/index.tsx
@@ -44,11 +44,11 @@ export const Employees = () => {
   }, []);
 
   return (
-    <>
-      <header className="fixed top-0 left-0 w-full h-[3.75rem] bg-white flex items-center px-4 shadow-md xl:mb-[2em]">
+    <div>
+      <header className="absolute top-0 left-0 w-full h-[3.75rem] bg-white flex items-center px-4 shadow-md xl:mb-[2em] z-0">
         <img src={Logo.logo} alt={Logo.title} />
       </header>
-      <main className="w-screen flex-col items-center justify-center mt-[5em] md:mt-[3em] xl:mt-[6em] px-8 lg:px-8 xl:px-10 pb-20">
+      <main className="w-screen flex-col items-center justify-center pt-20 pb-20 px-8 lg:px-8 xl:px-10">
         <div className="flex flex-col md:flex-row items-center justify-between mb-[1.5em] md:mb-[2em] gap-2 sm:gap-0">
           <h1 className="text-black text-xl mb-1 md:mb-0 font-semibold mr-auto md:ml-0">
             Funcionários
@@ -57,6 +57,6 @@ export const Employees = () => {
         </div>
         <TableEmployees employees={filteredEmployees} />
       </main>
-    </>
+    </div>
   );
 };
